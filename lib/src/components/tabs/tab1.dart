e8fda6db6d1369d9d8a36a60e10a51c53c8c261a
Nam Truong <nam.tt@neo-lab.vn>
2019-02-07 10:10:03 +0700

    init storage bucket

--- lib/src/components/tabs/tab1.dart
+++ lib/src/components/tabs/tab1.dart
@@ -2,11 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:study_flutter/src/components/post/post.dart';
 
 class Tab1 extends StatelessWidget {
+  final PageStorageBucket bucket;
+
+  Tab1({Key key, @required this.bucket}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: PageStorageKey('Tab1'),
       resizeToAvoidBottomPadding: false,
-      body: PostPage(),
+      body: PostPage(
+        bucket: this.bucket,
+      ),
     );
   }
 }
